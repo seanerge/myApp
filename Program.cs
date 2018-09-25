af6ef1a89e5e4a203b59efb72968d9c07f439cf7
Sean Chen <seanerge@gmail.com>
2018-09-25 21:11:54 +0800

    test commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Newegg!");
         }
     }
 }
